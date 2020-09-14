0c5ee6607cda918cdc6cd8e8c065361cfa648195
verminoid <verminoid@yandex.ru>
2020-09-14 11:34:09 +0300

    master

--- Blocks/Blocks.cpp
+++ Blocks/Blocks.cpp
@@ -4,10 +4,28 @@
  *    Масса блоков
  * 
  * *******************************/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    uint32_t n;
+    uint16_t r; 
+    cin >> n >> r;
+    uint32_t wight_Sum = 0;
+    for (uint32_t i = 0; i < n; i++)
+    {
+        uint32_t w,h,d;
+        cin >> w >> h >> d;
+        wight_Sum += (w*h*d)*r;
+    }
+    cout << wight_Sum << endl;
+
     return 0;
 }
  
